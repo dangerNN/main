fd93abff7d7170582b5be474aef916e0835e1303
DaniiLord <daniilsveckarev07@gmail.com>
2025-03-30 12:57:07 +0300

    нормальные коментарии

--- main/OrdersSystem.cpp
+++ main/OrdersSystem.cpp
@@ -13,17 +13,17 @@ void OrdersSystem::sortOrdersByReadyTime() {
 
 void OrdersSystem::completeFirstOrder() {
     if (orders.empty()) {
-        cout << "Немає замовлень для виконання!\n";
+		cout << "Немає замовлень для виконання!\n"; // почемуто гитхаб не то что тут написано "Немає замовлень для виконання"
         return;
     }
     orders.front().printOrder();
     orders.erase(orders.begin());
-    cout << "Перше замовлення виконано і видалено!\n";
+    cout << "Перше замовлення виконано і видалено!\n"; // почемуто гитхаб не то что тут написано "Перше замовлення виконано і видалено"
 }
 
 void OrdersSystem::printAllOrders() const {
     if (orders.empty()) {
-        cout << "Немає активних замовлень.\n";
+        cout << "Немає активних замовлень.\n"; // почемуто гитхаб не то что тут написано "Немає активних замовлень"
         return;
     }
     for (const auto& order : orders) {
